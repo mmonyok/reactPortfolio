0bd593a4ad639d3a33830c13f2f22aadb2bf2905
Ody <melodymonyok@gmail.com>
2021-09-13 21:56:00 -0500

    Imported necessary components and added renderPage function.

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    // if (currentPage === 'About') {
+    //   return <About />;
+    // }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <About />;
+  }
+
+}
