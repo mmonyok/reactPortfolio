d9febf43f450ffde3281b2e2d8cd6a3e3157c3ef
Ody <melodymonyok@gmail.com>
2021-11-01 21:49:21 -0500

    Added the client path to server.js.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require("./controllers");
 const cors = require('cors');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 
 // finish setting up router: https://w3collective.com/react-contact-form/
@@ -13,5 +14,13 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
