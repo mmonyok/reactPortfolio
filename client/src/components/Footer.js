a3eea11220720280ba8dffe7067869b3fb2e1bc7
Ody <melodymonyok@gmail.com>
2021-11-07 16:42:32 -0600

    Updated footer to be mobile friendly.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -26,13 +26,16 @@ const links = [
 
 function Footer() {
   return (
-    <footer className="footer text-center container-fluid border p-2 font-heavy mt-5">
+    <footer className="footer text-center container-fluid border p-2 font-heavy">
+      <hr />
       <div>
-        <ul className="d-inline-flex flex-row align-items-center mt-2">
-          {links.map((link, i, b, l) => (
-            <li key={i} className="border rounded-3 pe-2 mx-2 linkShadow">
-              <a key={b} className="my-auto" href={link.website} target="_blank" rel="noreferrer">
-                <i key={l} className={`h-100 p-2 rounded-start fab ${link.icon} fa-2x`}></i> {link.name}</a></li>
+        <ul className="d-inline-flex flex-row align-items-center mt-2" id="iconLinks">
+          {links.map((link, i, b, l, z) => (
+            <li key={i} className="border rounded-3 pe-2 mx-2 linkShadow linkContainer" id="linkContainer">
+              <a key={b} className="socialLink" href={link.website} target="_blank" rel="noreferrer">
+                <i key={l} className={`me-2 h-100 p-2 rounded-start fab ${link.icon} fa-2x`} id="linkIcon"></i></a>
+              <a key={z} className="my-auto linkText" href={link.website} target="_blank" rel="noreferrer">{link.name}</a>
+            </li>
           ))}
         </ul>
       </div>
